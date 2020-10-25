3b77f9b9ae8ff06da52617a051c0434335788059
Jennie Masterman <JRM12188@gmail.com>
2020-10-25 17:36:26 -0500

    update

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "bb1131fe18898ad249e2c4f1eee02d67"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyD26e5Jofbbmkbmi43hUnTCrUHbf3-T_PE"
